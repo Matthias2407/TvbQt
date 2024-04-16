0ac9b6de8337f209541702fe972abf660e2c95e5
Matthias2407 <45603960+Matthias2407@users.noreply.github.com>
2024-04-17 01:29:06 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+/*===LANGUAGES===*/
+    QTranslator translator;
+
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "TVB_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+/*===/LANGUAGES===*/
+
+    MainWindow w;
+    w.show();
+
+    return a.exec(); //that's like the main loop and waiting for event in QT, I guess
+}
